81712763f7164d26124689ec7e618d36b19bac25
navneet <nav700neet@gmail.com>
2025-01-29 22:48:05 +0530

    wrap up

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <img style="border-radius : 10px;" src="app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" alt="Logo" width="70" height="70">
+  <h3 align="center">QR Scanner App</h3>
+<p align="center">QR Scanner App using ML Kit Barcode Scanning API</p>
+</p>
+
+
+## Screenshots
+
+<p align="center">
+  <img src="assets/Screenshot_20250129_223743.png" alt="Screenshot 1" width="200">
+  <img src="assets/Screenshot_20250129_223904.png" alt="Screenshot 2" width="200">
+</p>
+
+
+<a href="https://drive.google.com/file/d/1f8OpAFNreawmnZASTC3d-7kDaMZuE7zr/view?usp=sharing" ><img src="https://img.icons8.com/color/48/000000/download" alt="download" width="50" height="50"/>  Download Game</a>
--- app/src/main/java/com/android/app/qrscanner/MainActivity.kt
+++ app/src/main/java/com/android/app/qrscanner/MainActivity.kt
@@ -2,6 +2,8 @@ package com.android.app.qrscanner
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -17,14 +19,44 @@ import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+import com.android.app.qrscanner.ui.theme.CameraRim
 import com.android.app.qrscanner.ui.theme.QRScannerTheme
+import com.android.app.qrscanner.ui.theme.Rim1
+import com.android.app.qrscanner.ui.theme.Rim2
+import com.android.app.qrscanner.ui.theme.Rim3
+import com.android.app.qrscanner.ui.theme.Rim4
+import com.android.app.qrscanner.ui.theme.Rim5
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
 import java.util.concurrent.ExecutorService
@@ -33,7 +65,6 @@ import java.util.concurrent.Executors
 class MainActivity : ComponentActivity() {
     private val cameraExecutor = Executors.newSingleThreadExecutor()
 
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -44,8 +75,52 @@ class MainActivity : ComponentActivity() {
             if (isGranted) {
                 setContent {
                     QRScannerTheme {
-                        Scaffold(modifier = Modifier.fillMaxSize()) {
-                            CameraPreview(cameraExecutor)
+                        var showDialog by remember {
+                            mutableStateOf("")
+                        }
+                        Scaffold(modifier = Modifier.fillMaxSize()) { it ->
+                            CameraPreview(cameraExecutor, it){ result ->
+                                showDialog = result
+                            }
+                            if (showDialog != "") {
+                                Dialog(
+                                    onDismissRequest = {
+                                        showDialog = ""
+
+                                    }
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        verticalArrangement = Arrangement.Center
+                                    ) {
+                                        Text(
+                                            text = "Result",
+                                            fontWeight = FontWeight.Medium,
+                                            color = Color.White,
+                                            modifier = Modifier.padding(10.dp)
+                                        )
+                                        Text(
+                                            text = showDialog,
+                                            color = Color.White,
+                                            modifier = Modifier.padding(10.dp)
+                                        )
+
+                                        Button(
+                                            onClick = {
+                                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(showDialog))
+                                                startActivity(intent)
+                                            },
+                                            colors = ButtonDefaults.buttonColors(
+                                                containerColor = Rim1,
+                                                contentColor = Color.White
+                                            )
+                                        ) {
+                                            Text("Browse")
+                                        }
+                                    }
+
+                                }
+                            }
                         }
                     }
                 }
@@ -63,40 +138,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CameraPreview(cameraExecutor: ExecutorService) {
+fun CameraPreview(cameraExecutor: ExecutorService, paddingValues: PaddingValues, onScanned: (String) -> Unit) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
 
-    AndroidView(
-        factory = { ctx ->
-            val previewView = PreviewView(ctx)
-            val cameraProvider = cameraProviderFuture.get()
+    Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+        AndroidView(
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                val cameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder().build().also {
-                it.surfaceProvider = previewView.surfaceProvider
-            }
+                val preview = Preview.Builder().build().also {
+                    it.surfaceProvider = previewView.surfaceProvider
+                }
 
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-            val imageAnalyzer = ImageAnalysis.Builder().build().also { it ->
-                it.setAnalyzer(cameraExecutor, { imageProxy ->
-                    processImageProxy(imageProxy) { result ->
-                        Toast.makeText(ctx, result, Toast.LENGTH_SHORT).show()
-                    }
-                })
-            }
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                val imageAnalyzer = ImageAnalysis.Builder().build().also { it ->
+                    it.setAnalyzer(cameraExecutor, { imageProxy ->
+                        processImageProxy(imageProxy) { result ->
+                            onScanned(result)
+//                            Toast.makeText(ctx, result, Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }
 
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalyzer)
-            } catch (e: Exception) {
-                Toast.makeText(ctx, "Failed to bind camera use cases", Toast.LENGTH_SHORT).show()
-            }
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalyzer)
+                } catch (e: Exception) {
+                    Toast.makeText(ctx, "Failed to bind camera use cases", Toast.LENGTH_SHORT).show()
+                }
 
-            previewView
-        },
-        modifier = Modifier.fillMaxSize()
-    )
+                previewView
+            },
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        Box(modifier = Modifier.fillMaxSize().border(15.dp, Rim5, RoundedCornerShape(25.dp))) {}
+        Box(modifier = Modifier.fillMaxSize().padding(5.dp).border(6.dp, Color.White, RoundedCornerShape(31.dp))) {}
+        Box(modifier = Modifier.fillMaxSize().padding(10.dp).border(6.dp, Rim3, RoundedCornerShape(25.dp))) {}
+    }
 }
 
 @OptIn(ExperimentalGetImage::class)
